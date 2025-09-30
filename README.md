aa5e53fb4f71976a95aa096d24f6c662763c8c71
Snow <biteakar123@gmail.com>
2025-09-30 18:34:16 +0700

    commit lan 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# thuthanh2d
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/thuthanh2d/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.thuthanh2d
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
